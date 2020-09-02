d0384b6d12e6dc43ff19c6f9d8636c132b622737
Ali Toyn <ali.toyn@infinityworks.com>
2020-09-02 20:05:55 +0100

    basic connection established

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-let config = require('./config');
+// https://blog.petehouston.com/remote-command-execution-via-ssh-using-nodejs/
+const config = require('./config');
+const SSH = require('simple-ssh');
 
-console.log(config.test)
+let ssh = new SSH({
+    host: config.host,
+    user: config.username,
+    pass: config.password
+});
+
+ssh.exec('df -h /', {
+    out: function(stdout) { console.log(stdout); },
+    err: function(stderr) { console.log(stderr); },
+    exit: function(code) { console.log(code); }
+}).start();
