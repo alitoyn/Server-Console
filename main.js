ac6d6eeb832183795bd43a9103cea7ac10dde7cf
Ali Toyn <ali.toyn@infinityworks.com>
2020-09-05 20:14:42 +0100

    basic call to local api

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
-// https://blog.petehouston.com/remote-command-execution-via-ssh-using-nodejs/
-const config = require('./config');
-const SSH = require('simple-ssh');
-
-class Server {
-    constructor() {
-        let lastData = null
+$("#send").click(function () {
+    $.ajax({
+        url: "http://localhost:3001/hostname",
+        type: "GET",
+        crossDomain: true,
+        headers: {
+            "Authorization": "Bearer " + config.token, 
+          },
+        success: function (data, status) {
+            console.log(data)
+            // $("#container").text(JSON.stringify(data.form))
     }
-
-    updateData(data) {
-        console.log(data)
-        this.lastData = data
-    }
-}
-
-let server = new Server
-
-let ssh = new SSH({
-    host: config.host,
-    user: config.username,
-    pass: config.password
+    });
 });
-
-ssh.exec('df -h /', {
-    out: function(stdout) {
-        server.updateData(stdout)
-            // console.log(stdout);
-    },
-
-    // err: function(stderr) { console.log(stderr); },
-    // exit: function(code) { console.log(code); }
-}).start();
-
-while (server.lastData === undefined) {
-    continue;
-}
-
-console.log(server.lastData + '_')
